4227aee0d320c0d0e87d0f400b7d4ce3d65e2ba5
njeri waweru <wawerunjeri714@gmail.com>
2022-01-26 00:19:42 +0300

    commits

---
+++ dictonary1.py
@@ -0,0 +1,8 @@
+# Creating empty dictionaries
+my_dict = {}
+my_dict = dict()
+
+# Creating a dictionary with keys and values
+my_cat = {'name':'Mr sniffles','age':18, 'color':'black'}
+cat_age = my_cat['age']
+print(cat_age)
---
+++ dictonary2.py
@@ -0,0 +1,8 @@
+birthdays = {"John":"August 1","Marcus":"April 8"}
+birthdays["mary"] = "September 14"
+print(birthdays) # this prints {"John":"August 1","Marcus":"April 8","Mary":"September 14"}
+birthday = {"John":"August 1","Marcus":"April 8","Mary":"September 14"}
+
+# print(birthday.keys())
+
+print(list(birthday.keys()))
